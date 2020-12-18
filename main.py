14d1f8b122a1be5c2f1f9fa8a414c80a36f4c14c
PleaseGiveMeReset <76155806+PleaseGiveMeReset@users.noreply.github.com>
2020-12-18 04:18:00 -0500

    12/17/2020

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import time
 from pynput.keyboard import Controller
 import PySimpleGUI as sg
 import serial.tools.list_ports
+joyN = 'up'
+joyS = 'down'
+joyE = 'left'
+joyW = 'right'
+btn1 = 'a'
+btn2 = 's'
+btn3 = 'd'
+btn4 = 'f'
+btn5 = 'z'
+btn6 = 'x'
+btn7 = 'c'
+btn8 = 'v'
 
 availablePorts = list(serial.tools.list_ports.comports())
 availablePortsList = {}
@@ -12,12 +24,17 @@ for x in availablePorts:
     AP += 1
 keyboard = Controller()
 setup = True
-# noinspection PyTypeChecker
-layout = [[sg.Text("Please Select an Available COM Port:")],
+layoutSetup = [[sg.Text("Please Select an Available COM Port:")],
           [sg.Text(availablePortsList)],
           [sg.Combo(['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8'], enable_events=True, key=1)],
           [sg.Button('OK')]]
-setupWindow = sg.Window('ArduinoStick 1.0', layout)
+layoutConfig = [[sg.Text(f'{element} -'), sg.In(key=element, size=(2,0)), sg.Text(joyE)] for element in 'NSE']
+layoutConfig += [[sg.Text(f'{element}-'), sg.In(key=element, size=(2,0)), sg.Text(joyE)] for element in 'W']
+layoutConfig += [[sg.Text(f'{i} -'), sg.In(key=i, size=(2,0))] for i in range(1,9)]
+
+
+setupWindow = sg.Window('ArduinoStick 1.0', layoutSetup)
+configWindow = sg.Window('ArduinoStick 1.0', layoutConfig)
 
 time.sleep(3)
 
@@ -33,7 +50,7 @@ while True:
             break
 
     while not setup:
-        event, values = setupWindow.read()
+        event, values = configWindow.read()
         # noinspection PyUnboundLocalVariable
         data = arduino.read()
         print(data)
